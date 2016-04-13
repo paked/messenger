f98f5920a3ce2d777342b541b37969895d101904
Harrison <harrison@theshoebridges.com>
2016-04-14 09:47:15 +1000

    Add documentation of actions

--- actions.go
+++ actions.go
@@ -1,9 +1,14 @@
 package messenger
 
+// Action is used to determine what kind of message a webhook event is.
 type Action int
 
 const (
+	// UnknownAction means that the event was not able to be classified.
 	UnknownAction Action = iota - 1
+	// TextAction means that the event was a text message (May contain attachments).
 	TextAction
+	// DeliveryAction means that the event was a previous recipient reading their respective
+	// messages.
 	DeliveryAction
 )
