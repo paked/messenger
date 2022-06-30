4f279cdecec1df176f35c9f46587a16275e664da
simus <sirojiddinmustafayev@gmail.com>
2022-06-30 13:40:12 +0500

    added additional fields for instagram users, to Profile struct

--- profile.go
+++ profile.go
@@ -9,4 +9,10 @@ type Profile struct {
 	Locale        string  `json:"locale"`
 	Timezone      float64 `json:"timezone"`
 	Gender        string  `json:"gender"`
+
+	// instagram user profile
+	Username        string `json:"username,omitempty"`
+	IsPrivate       bool   `json:"is_private,omitempty"`
+	FollowCount     int32  `json:"follow_count,omitempty"`
+	FollowedByCount int32  `json:"followed_by_count,omitempty"`
 }
